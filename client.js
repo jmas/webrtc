f587dfc3b27bd11eee882b7db6e60f0a283294a7
jmas <jmas.ukraine@gmail.com>
2021-02-28 20:54:01 +0200

    Add wss

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ var name;
 var connectedUser;
 
 //connecting to our signaling server
-var conn = new WebSocket("ws://192.168.0.103:9090");
+var conn = new WebSocket("wss://192.168.0.103:9090");
 
 conn.onopen = function () {
   console.log("Connected to the signaling server");
